51da29dc5c15272528d87c5fa292c37a1231d074
mnk44 <mnkmm44@gmail.com>
2023-02-23 03:12:44 -0500

    [Feature]: Basic types of declarations

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,11 @@
-
-
-console.log('Hola Mundo!');
-
 /*
     ===== Código de TypeScript =====
 */
 
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = false;
+
+hp = 'FULL';
+
+console.log(nombre, hp);
